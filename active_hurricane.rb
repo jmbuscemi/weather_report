c47427f2233ec2c5e749ce5cdb43602d169c4020
John M Buscemi <jmbuscemi@gmail.com>
2015-09-21 23:35:14 -0400

    TODO: Refactor active_hurricane.rb

--- active_hurricane.rb
+++ active_hurricane.rb
@@ -3,18 +3,72 @@ require 'httparty'
 class ActiveHurricane
 
   def initialize
-    @hurricane = get_data
+    @hurricane = get_data["currenthurricane"]
   end
 
-  def get_hurricane
-    array = []
-    array = @hurricane["currenthurricane"]
+  # def name
+  #   return "There are currently no hurricanes." if @hurricane == []
+  #   # self.no_hurricanes?
+  #   output = @hurricane.map {|item| "Hurricane Name: #{item["stormInfo"]["stormName_Nice"]}"}
+  # end
+  #
+  # def category
+  #   return "There are currently no hurricanes." if @hurricane == []
+  #   # self.no_hurricanes?
+  #   output = @hurricane.map do |item|
+  #     "Category: #{item["Current"]["SaffirSimpsonCategory"]} (#{item["Current"]["Category"]})"
+  #   end
+  # end
+  #
+  # def sustained_winds
+  #   return "There are currently no hurricanes." if @hurricane == []
+  #   # self.no_hurricanes?
+  #   output = @hurricane.map do |item|
+  #     "Sustained Winds: #{item["Current"]["WindSpeed"]["Mph"]}mph"
+  #   end
+  # end
+  #
+  # def wind_gust
+  #   return "There are currently no hurricanes." if @hurricane == []
+  #   # self.no_hurricanes?
+  #   output = @hurricane.map do |item|
+  #     "Max Wind Gust: #{item["Current"]["WindGust"]["Mph"]}mph" unless item["Current"]["WindGust"]["Mph"] == "null"
+  #   end
+  # end
+  #
+  # def latitude
+  #   return "There are currently no hurricanes." if @hurricane == []
+  #   # self.no_hurricanes?
+  #   output = @hurricane.map do |item|
+  #     if item["Current"]["lat"] < 0
+  #       "Location: #{item["Current"]["lat"]*(-1)}deg S"
+  #     else
+  #       "Location: #{item["Current"]["lat"]}deg N"
+  #     end
+  #   end
+  # end
+  #
+  # def longtitude
+  #   return "There are currently no hurricanes." if @hurricane == []
+  #   # self.no_hurricanes?
+  #   output = @hurricane.map do |item|
+  #     if item["Current"]["lon"] < 0
+  #       "Location: #{item["Current"]["lon"]*(-1)}deg W"
+  #     else
+  #       "Location: #{item["Current"]["lon"]}deg E"
+  #     end
+  #   end
+  # end
 
-    if array == []
+  def get_hurricane
+    # array = []
+    # array = @hurricane["currenthurricane"]
+
+    if @hurricane == []
       puts "There are currently no hurricanes."
       storm_name = "NONE"
     else
-      array.each_with_index do |item, index|
+      output = @hurricane.map do |item|
         puts "Hurricane Name: #{item["stormInfo"]["stormName_Nice"]}"
         puts "Category: #{item["Current"]["SaffirSimpsonCategory"]} (#{item["Current"]["Category"]})"
         puts "Sustained Winds: #{item["Current"]["WindSpeed"]["Mph"]}mph"
@@ -39,6 +93,7 @@ class ActiveHurricane
   end
 
   private def get_data
-    HTTParty.get("https://api.wunderground.com/api/#{ENV["WUNDERGROUND_KEY"]}/currenthurricane/view.json")
+    # HTTParty.get("https://api.wunderground.com/api/#{ENV["WUNDERGROUND_KEY"]}/currenthurricane/view.json")
+    JSON.parse(File.read("./test_input/hurricane.json"))
   end
 end
